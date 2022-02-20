945abdce80aff88b3a4c7473579a0b12b2dc2e3f
Moses Kamau <moseskamau338@gmail.com>
2022-02-20 12:06:45 +0300

    Web file refactored, Sale stats added

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -9,12 +9,39 @@ class SaleController extends Controller
 {
     public function index()
     {
-        //fetch sales:
-//        $sales = Sale::with('zone:id,name','vehicle:id,plate_no,name', 'customer:id,name','user:id,name','rate:id,amount','gateway:id,name')
-//        ->paginate(5);
 
-        //massage
-        return view('sales.index');
+        $data = (object)[
+            'paid'=>(object)[
+                'count'=>0,
+                'sum'=>0,
+            ],
+            'pending'=>(object)[
+                'count'=>0,
+                'sum'=>0,
+            ],
+            'lost'=>(object)[
+                'count'=>0,
+                'sum'=>0,
+            ],
+        ];
+//        ['PAID','LOSS','PENDING']
+        foreach (['PAID','LOSS','PENDING'] as $status) {
+            $q = Sale::query()->where('status', $status);
+//            dd($status);
+            if ($status === 'PAID'){
+                $data->paid->count = $q->count();
+                $data->paid->sum = $q->sum('totals');
+            }
+            if ($status === 'PENDING'){
+                $data->pending->count = $q->count();
+                $data->pending->sum = $q->sum('totals');
+            }
+            if ($status === 'LOSS'){
+                $data->lost->count = $q->count();
+                $data->lost->sum = $q->sum('totals');
+            }
+        }
+        return view('sales.index', compact('data'));
     }
 
     public function handovers(Request $request)
